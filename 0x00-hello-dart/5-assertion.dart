1cfe9869561bb237e293047a4bd00038ff98c09a
fernan17893 <fernan17893@gmail.com>
2023-11-08 12:41:47 -0400

    last tasks

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+    var n = int.parse(args[0]);
+    try {
+        assert(n >= 80, 'Uncaught Error: Assertion failed: The score must be bigger or equal to  80');
+        print('You Passed');
+    } on AssertionError {
+        print('You Failed');
+    }
+}
