06c4c1e8f3f34b46cec92ce09dff943856fd89f6
fernan17893 <fernan17893@gmail.com>
2023-11-08 12:44:59 -0400

    tsak 5 fix

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -3,7 +3,5 @@ void main(List<String> args) {
     try {
         assert(n >= 80, 'Uncaught Error: Assertion failed: The score must be bigger or equal to  80');
         print('You Passed');
-    } on AssertionError {
-        print('You Failed');
     }
 }
