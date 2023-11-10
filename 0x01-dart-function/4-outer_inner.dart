201e5ab3a4b281de5f75c39ae922915809a8d473
fernan17893 <fernan17893@gmail.com>
2023-11-10 11:51:54 -0400

    task 4 spacing issue

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,7 @@
 void outer(String name, String id) {
     String inner() {
         var split = name.split(" ");
-        var named = split[1].substring(0, 1) + ". " + split[0];
+        var named = split[1].substring(0, 1) + "." + split[0];
         return "Hello Agent ${named} your id is ${id}";
     }
     print (inner());
