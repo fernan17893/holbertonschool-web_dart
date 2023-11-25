26bb327e35f8b89da7a120ca1712e4a29a7bcd28
fernan17893 <fernan17893@gmail.com>
2023-11-25 14:49:28 -0400

    gotnomoney getsum gotnomoney get sum

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,22 @@
+import '4-util.dart';
+import 'dart:convert';
+
+
+Future<double> calculateTotal() async {
+    try {
+    return fetchUserData().then((data) {
+        String id = json.decode(data)['id'];
+        return fetchUserOrders(id).then((orders) async {
+            double total = 0.0;
+            for (String order in json.decode(orders)) {
+                await fetchProductPrice(order).then((price) {
+                    total += json.decode(price);
+                });
+            }
+            return total;
+        });
+    });
+    } catch (err) {
+        return 0.0;
+    }  
+}
