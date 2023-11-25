d7aff77c6e36a4b82574e53f925b83758be4cab7
fernan17893 <fernan17893@gmail.com>
2023-11-25 12:46:57 -0400

    task 3 repush

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,27 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+    try {
+        var user = await fetchUserData();
+        var id = json.decode(user)['username'];
+        return 'Hello $id';
+    } catch (err) {
+        print ('error caught: $err');
+        throw "";
+    }
+}
+
+Future<String> loginUser() async {
+    try {
+        if (await checkCredentials()) {
+            print("There is a user: true");
+            return await greetUser();
+        } else {
+            print("There is a user: false");
+            return 'Could not log in';
+        }
+    } catch (err) {
+        return 'error caught: $err';
+    }
+}
